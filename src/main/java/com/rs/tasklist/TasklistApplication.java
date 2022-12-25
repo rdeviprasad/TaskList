7190514fd5f1aa7eb0eeebf5e56da75bd257cfec
rdeviprasad <r.s.devi.prasad@gmail.com>
2022-12-25 22:50:27 +0530

    test commit

--- src/main/java/com/rs/tasklist/TasklistApplication.java
+++ src/main/java/com/rs/tasklist/TasklistApplication.java
@@ -1,7 +1,18 @@
 package com.rs.tasklist;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class TasklistApplication {
@@ -10,4 +21,37 @@ public class TasklistApplication {
 		SpringApplication.run(TasklistApplication.class, args);
 	}
 
+}
+
+@Component
+@RequiredArgsConstructor
+class SampleDataInitializer {
+
+	private final TaskRepository taskRepository;
+
+	@EventListener(ApplicationEvent.class)
+	public void initialize() {
+		Flux<Task> tasks = Flux
+			.just("Post on IG", "Write the blog", "Complete side project")
+			.map(task -> new Task(null, task))
+			.flatMap(taskRepository::save);
+
+
+	}
+}
+
+interface TaskRepository extends ReactiveCrudRepository<Task, String> {
+
+}
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Task {
+
+	@Id
+	private String id;
+
+	private String name;
 }
