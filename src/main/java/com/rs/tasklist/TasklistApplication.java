1d3f19d38602450e1c1fc7b2dfd0cf291c870dd8
rdeviprasad <r.s.devi.prasad@gmail.com>
2023-01-15 15:12:31 +0530

    add post and get apis for task list app

--- src/main/java/com/rs/tasklist/TasklistApplication.java
+++ src/main/java/com/rs/tasklist/TasklistApplication.java
@@ -1,18 +1,28 @@
 package com.rs.tasklist;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import java.util.function.Consumer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationEvent;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class TasklistApplication {
@@ -23,20 +33,46 @@ public class TasklistApplication {
 
 }
 
+@RestController
+@RequiredArgsConstructor
+class TaskRestController {
+	private final TaskRepository taskRepository;
+
+	@GetMapping("/tasks")
+	Publisher<Task> getTasks() {
+		return this.taskRepository.findAll();
+	}
+
+	@PostMapping("/tasks")
+	Mono<Void> saveTask(@RequestBody Task task) {
+		return this.taskRepository.save(task).then();
+	}
+}
+
 @Component
 @RequiredArgsConstructor
+@Slf4j
 class SampleDataInitializer {
 
 	private final TaskRepository taskRepository;
 
-	@EventListener(ApplicationEvent.class)
+	@EventListener(ApplicationReadyEvent.class)
 	public void initialize() {
-		Flux<Task> tasks = Flux
+		var tasks = Flux
 			.just("Post on IG", "Write the blog", "Complete side project")
 			.map(task -> new Task(null, task))
 			.flatMap(taskRepository::save);
 
-
+		taskRepository
+			.deleteAll()
+			.thenMany(tasks)
+			.thenMany(taskRepository.findAll())
+			.subscribe(new Consumer<Task>() {
+				@Override
+				public void accept(Task task) {
+					log.info("task : {}", task);
+				}
+			});
 	}
 }
 
@@ -51,7 +87,7 @@ interface TaskRepository extends ReactiveCrudRepository<Task, String> {
 class Task {
 
 	@Id
+	@JsonSerialize(using= ToStringSerializer.class)
 	private String id;
-
 	private String name;
 }
